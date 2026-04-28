1f3c7e587b1dd509a77f165291cdd7e7080da9fa
sheeraanela <sheeraanela24@gmail.com>
2026-04-28 09:36:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,6 @@ st.markdown("""
     .stTabs [aria-selected="true"] { background-color: #CC0000 !important; color: #FFFFFF !important; }
     #MainMenu {visibility: hidden;} footer {visibility: hidden;} header {visibility: hidden;}
     button[data-testid="baseButton-headerNoPadding"] { display: none !important; }
-    [data-testid="collapsedControl"] { display: none !important; }
 </style>
 """, unsafe_allow_html=True)
 
